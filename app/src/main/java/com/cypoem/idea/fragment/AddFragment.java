e76655b79882f1dd8be63f23be75345fb06f38b8
zhpan <zhpanvip@outlook.com>
2017-04-21 20:18:27 +0800

    add Fragment

---
+++ app/src/main/java/com/cypoem/idea/fragment/AddFragment.java
@@ -0,0 +1,20 @@
+package com.cypoem.idea.fragment;
+
+import com.airong.core.BaseRxFragment;
+import com.cypoem.idea.R;
+
+/**
+ * Created by zhpan on 2017/4/21.
+ */
+
+public class AddFragment extends BaseRxFragment {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_add;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
---
+++ app/src/main/java/com/cypoem/idea/fragment/MessageFragment.java
@@ -0,0 +1,20 @@
+package com.cypoem.idea.fragment;
+
+import com.airong.core.BaseRxFragment;
+import com.cypoem.idea.R;
+
+/**
+ * Created by zhpan on 2017/4/21.
+ */
+
+public class MessageFragment extends BaseRxFragment {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_message;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
